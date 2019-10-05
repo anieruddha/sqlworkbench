8897147145bca09e30d47f2165355431d5160ff2
Aniruddha Gaikwad <aniruddha.gaikwad@skipthedishes.ca>
2019-10-05 15:24:28 -0500

    if os == mac then try to set global menu without overriding lookNfeel

--- src/workbench/gui/WbUIManager.java
+++ src/workbench/gui/WbUIManager.java
@@ -6,7 +6,8 @@ import workbench.util.Os;
 public class WbUIManager extends UIManager {
     public static void setLookAndFeel(LookAndFeel newLookAndFeel) throws UnsupportedLookAndFeelException {
         if(Os.getCurrentOs() == Os.Mac){
-            // show menus on mac top bar
+            // show menus on mac top bar, this may override by Look-N-Feel
+            System.setProperty("com.apple.macos.useScreenMenuBar", "true" );
             System.setProperty("apple.laf.useScreenMenuBar", "true");
         }
         UIManager.setLookAndFeel(newLookAndFeel);
